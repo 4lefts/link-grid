7420b8d0c72f38e48cdb78375f73e9d52d3a7614
4lefts <5t3ph3n.ball@gmail.com>
2021-10-10 15:41:54 +0100

    updated links and bg

--- src/App.svelte
+++ src/App.svelte
@@ -4,31 +4,27 @@
   import Logo from "./logo.svelte";
   import Linkgrid from "./linkgrid.svelte";
   import Admin from "./admin.svelte";
-  import Bgcredit from "./bgcredit.svelte";
 
   const routes = {
     "/": Linkgrid,
     "/admin": Admin,
     "*": Linkgrid,
   };
+
+  let bgImgUrl = "https://source.unsplash.com/yuiJO6bvHi4/1280x720";
 </script>
 
-<div class="app">
+<div class="app" style="background-image: url({bgImgUrl})">
   <header>
     <Logo size="80" color="white" />
-    <h3>Decoy School Links {$location === "/admin" ? "(Admin Page)" : ""}</h3>
-    <h1>Hello</h1>
+    <h1>Decoy School Links {$location === "/admin" ? "(Admin Page)" : ""}</h1>
   </header>
   <Router {routes} />
-  <Bgcredit
-    creditUrl={`https://unsplash.com/@yoksel?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Yoksel 🌿 Zok</a> on <a href="https://unsplash.com/s/photos/daffodils?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText`}
-    photographerName={`Yoksel Zok`}
-  />
 </div>
 
 <style>
   .app {
-    background-image: url("/bgs/spring2021bg.jpg");
+    background-color: #1d1d1d;
     min-height: 100%;
     background-position: center;
     background-repeat: no-repeat;
@@ -40,19 +36,15 @@
     max-width: 960px;
     margin: 0 auto;
     padding: 1.3em 0 0.7em 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
   }
-  h3,
   h1 {
     font-family: "Quicksand", sans-serif;
     color: white;
     margin: 0;
     text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.8);
-  }
-  h3 {
-    font-size: 1.8em;
-    margin-bottom: 0.3em;
-  }
-  h1 {
-    font-size: 2.6em;
+    font-size: 2.2em;
   }
 </style>
